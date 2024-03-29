53ab77e52bc3a4f1f3d4b8ea12d476cad04bd096
SunYoung Park <youngkairos98@gmail.com>
2024-03-30 02:56:11 +0900

    Baekjoon 1003.py

--- Baekjoon/1003.py
+++ Baekjoon/1003.py
@@ -1,25 +1,19 @@
 import sys
 sys.stdin = open("Baekjoon/test.txt", "r")
 
-t = int(input())
+#호출되는 동적테이블 만들기
+dp = [[0, 0] for _ in range(41)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+
+for i in range(2, 41) :
+    dp[i][0] = dp[i-1][0] + dp[i-2][0]
+    dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+## 조건에 따라 테이블에서 값 찾기
+t = int(sys.stdin.readline().rstrip())
 
 for i in range(t) :
-    n = int(input())
-    dp = [0] * (n+2)
+    n = int(sys.stdin.readline())
 
-    if n == 0 : 
-        dp[0] = 1
-    elif n == 1 :
-        dp[1] = 1
-
-    while n >= 2 :
-        dp[n-1] += 1 
-        dp[n-2] += 1
-        n -= 1
-        if n == 1 : 
-            break
-    
-    print(dp)
-    print(dp[0], dp[1])
-
-
+    print(dp[n][0], dp[n][1])
